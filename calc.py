f3f7e792dcfdd13af5e61f6d31155ba653fbbd4d
Bill0424 <57516119+Bill0424@users.noreply.github.com>
2025-05-06 14:57:23 +0800

    change the function from square to adder

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
 # Simple calculation: square the number
 try:
-    num = float(input("Enter a number: "))
-    print(f"The square of {num} is {num ** 2}")
+    print("Add two number")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(f"The sum of {num1} + {num2} is {num1+num2}")
 except ValueError:
     print("Invalid input. Please enter a number.")
